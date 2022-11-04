1557d5a5692e0bd25790fa24ab8e4810fba57580
Humber Sanchez Nieto <husani925@gmail.com>
2022-11-04 14:53:05 -0500

    add to list faq

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -8,14 +8,14 @@ export default function Faq() {
                 Frequently Asked Questions
             </h2>
 
-            <p className="pt-6 pb-6 text-base max-w-2xl text-center m-auto">
+            <p className="pt-6 pb-16 text-base max-w-2xl text-center m-auto">
                 Answer your customers possible questions here, it will increase the conversion rate as well as support or chat requests.
             </p>
             <div className="mx-auto w-full max-w-2xl rounded-2xl bg-white">
                 <Disclosure>
                     {({ open }) => (
                         <>
-                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-50 px-4 py-3 text-left text-base font-medium hover:bg-gray-100">
+                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-50 px-4 py-4 text-left text-base font-medium hover:bg-gray-100">
                                 <span>What is your refund policy?</span>
                                 <RiArrowUpSLine
                                     className={`${open ? 'rotate-180 transition-transform' : ''
@@ -32,7 +32,7 @@ export default function Faq() {
                 <Disclosure as="div" className="mt-3">
                     {({ open }) => (
                         <>
-                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-50 px-4 py-3 text-left text-base font-medium hover:bg-gray-100">
+                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-50 px-4 py-4 text-left text-base font-medium hover:bg-gray-100">
                                 <span>Do you offer technical support?</span>
                                 <RiArrowUpSLine
                                     className={`${open ? 'rotate-180 transition-transform' : ''
@@ -48,7 +48,7 @@ export default function Faq() {
                 <Disclosure as="div" className="mt-3">
                     {({ open }) => (
                         <>
-                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-50 px-4 py-3 text-left text-base font-medium hover:bg-gray-100">
+                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-50 px-4 py-4 text-left text-base font-medium hover:bg-gray-100">
                                 <span>What is your refund policy?</span>
                                 <RiArrowUpSLine
                                     className={`${open ? 'rotate-180 transition-transform' : ''
@@ -57,6 +57,22 @@ export default function Faq() {
                             </Disclosure.Button>
                             <Disclosure.Panel className="px-4 pt-4 pb-2 text-base">
                                 If youre unhappy with your purchase for any reason, email us within 90 days and well refund you in full, no questions asked.
+                            </Disclosure.Panel>
+                        </>
+                    )}
+                </Disclosure>
+                <Disclosure as="div" className="mt-3">
+                    {({ open }) => (
+                        <>
+                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-50 px-4 py-4 text-left text-base font-medium hover:bg-gray-100">
+                                <span>What forms of payment do you accept?</span>
+                                <RiArrowUpSLine
+                                    className={`${open ? 'rotate-180 transition-transform' : ''
+                                        } h-5 w-5`}
+                                />
+                            </Disclosure.Button>
+                            <Disclosure.Panel className="px-4 pt-4 pb-2 text-base">
+                                You can use any debit or credit card to pay for a subscription. We secure your card details and process payments via Stripe.
                             </Disclosure.Panel>
                         </>
                     )}
