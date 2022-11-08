b72118c7251d9728426e95e37165f5991bffa706
Humber Sanchez Nieto <husani925@gmail.com>
2022-11-08 15:06:06 -0500

    more columns prices

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -4,28 +4,26 @@ import { FaCheck } from "react-icons/fa";
 
 const pricesmonth = [
     {
-        title: "LITE",
-        price: 49,
-        currency: "USD",
-        frecuency: "/ monthly",
+        title: "Hobby",
+        price: 12,
+        currency: "$",
+        frecuency: "/month",
         description: "All the features you need to grow your business.",
         features: [
             "Unlimited vouchers ∞",
             "02 users",
             "Point of sale",
             "Customers",
-            "Inventory",
-            "Reports",
         ],
-        cta: "Get started",
+        cta: "Buy Hobby",
         mostPopular: false,
     },
 
     {
-        title: "PRO",
-        price: 69,
-        currency: "USD",
-        frecuency: "/ monthly",
+        title: "Freelancer",
+        price: 24,
+        currency: "$",
+        frecuency: "/month",
         description: "All the features you need to grow your business.",
         features: [
             "Unlimited vouchers ∞",
@@ -33,20 +31,16 @@ const pricesmonth = [
             "02 warehouses",
             "Dashboard",
             "Point of sale",
-            "Inventory",
-            "Shopping",
-            "Accounting",
-            "Reports",
         ],
-        cta: "Get started",
+        cta: "Buy Freelancer",
         mostPopular: true,
     },
 
     {
-        title: "PREMIUM",
-        price: 99,
-        currency: "USD",
-        frecuency: "/ monthly",
+        title: "Startup",
+        price: 32,
+        currency: "$",
+        frecuency: "/month",
         description: "All the features you need to grow your business.",
         features: [
             "Unlimited vouchers ∞",
@@ -54,14 +48,30 @@ const pricesmonth = [
             "04 warehouses",
             "Dashboard",
             "Point of sale",
-            "Inventory",
-            "Shopping",
-            "Accounting",
-            "Reports",
             "Finance",
             "mobile App",
         ],
-        cta: "Get started",
+        cta: "Buy Startup",
+        mostPopular: false,
+    },
+    {
+        title: "Enterprice",
+        price: 48,
+        currency: "$",
+        frecuency: "/month",
+        description: "All the features you need to grow your business.",
+        features: [
+            "Unlimited vouchers ∞",
+            "16 users",
+            "08 warehouses",
+            "Dashboard",
+            "Point of sale",
+            "Inventory",
+            "Shopping",
+            "Finance",
+            "mobile App",
+        ],
+        cta: "Buy Enterprice",
         mostPopular: false,
     },
 ];
@@ -105,35 +115,46 @@ export default function Pricing() {
                 </button>
             </div>
 
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 pb-16 grid md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-16">
-
-
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 pb-16 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-16">
                 {pricesmonth.map((plan) => (
                     <div
                         key={plan.title}
-                        className={`rounded-2xl p-8 relative flex flex-col ${plan.mostPopular
+                        className={`rounded-lg py-8 relative flex flex-col ${plan.mostPopular
                             ? "border-rose-300 border-2 border-solid dark:border-rose-500"
                             : "border-gray-300 border"}      
                               `}>
-                        <h3 className="text-lg font-bold leading-5">
+                        <h3 className="px-6 text-lg font-semibold leading-5">
                             {plan.title}
                         </h3>
                         {plan.mostPopular && (
-                            <p className="absolute top-0 px-4 py-1 -translate-y-1/2 bg-rose-100 text-rose-500  rounded-full font-semibold tracking-wide shadow-md">
+                            <p className="mx-6 absolute top-0 px-4 py-1 -translate-y-1/2 bg-rose-100 text-rose-500  rounded-full text-sm font-semibold tracking-wide shadow-md">
                                 Most popular
                             </p>
                         )}
-                        <p className="mt-4  leading-6">{plan.description}</p>
-                        <div className="mt-6 rounded-lg bg-gray-100 dark:bg-neutral-800 p-6 -mx-6">
-                            <p className="text-sm font-semibold flex items-center">
-                                <span>{plan.currency}</span>
-                                <span className="text-4xl font-bold ml-3 ">{plan.price}</span>
+
+
+                        <p className="px-6 mt-4 leading-6">{plan.description}</p>
+                        <div className="mt-2 p-6 -mx-6">
+                            <p className="px-6 font-medium flex place-items-end">
+                                <span className="text-4xl font-bold">{plan.currency}</span>
+                                <span className="text-4xl font-bold">{plan.price}</span>
                                 <span className="ml-1.5">{plan.frecuency}</span>
                             </p>
                         </div>
-                        {/* features */}
 
-                        <ul className="mt-6 space-y-4 flex-1">
+                        {/* Call to action */}
+                        <Link
+                            href="#"
+                            className={`mt-4 mx-6 block px-6 py-3 font-medium leading-4 text-center rounded-lg ${plan.mostPopular
+                                ? "bg-rose-500 text-white shadow-md"
+                                : "bg-black text-white dark:bg-white dark:text-black"}      
+                                      `}>
+                            {plan.cta}
+                        </Link>
+
+                        {/* features */}
+                        <ul className="mt-6 px-6 space-y-4 flex-1 border-t border-gray-300">
+                            <p className="mt-6 font-semibold ">What&apos;s included</p>
                             {plan.features.map((feature) => (
                                 <li key={feature} className="leading-6 flex">
                                     <FaCheck className="mt-2 w-3 h-3 text-rose-500 shrink-0" />
@@ -141,15 +162,7 @@ export default function Pricing() {
                                 </li>
                             ))}
                         </ul>
-                        {/* Call to action */}
-                        <Link
-                            href="#"
-                            className={`  mt-8 block px-6 py-4 font-semibold leading-4 text-center rounded-full ${plan.mostPopular
-                                ? "bg-rose-500 text-white shadow-md"
-                                : "bg-black text-white dark:bg-white dark:text-black"}      
-                                      `}>
-                            {plan.cta}
-                        </Link>
+
                     </div>
                 ))}
             </div>
