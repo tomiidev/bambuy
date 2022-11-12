0ef09e4fe6ae11676381d4ab57f3748fda7ca03f
Humber Sanchez Nieto <husani925@gmail.com>
2022-11-12 18:02:55 -0500

    format

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useForm, useWatch } from "react-hook-form";
 
-import { HiOutlineLocationMarker } from "react-icons/hi"
-import { MdOutlineMail } from "react-icons/md"
-import { HiOutlinePhone } from "react-icons/hi"
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { MdOutlineMail } from "react-icons/md";
+import { HiOutlinePhone } from "react-icons/hi";
 
 export default function Contact() {
   const {
@@ -22,7 +22,7 @@ export default function Contact() {
   const userName = useWatch({
     control,
     name: "name",
-    defaultValue: "Someone"
+    defaultValue: "Someone",
   });
 
   setValue("subject", `${userName} envió un mensaje desde el sitio web Halley`);
@@ -58,19 +58,21 @@ export default function Contact() {
   return (
     <div className="bg-gray-50 dark:bg-neutral-800" id="contact">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 text-center">
-        <h2 className="text-4xl font-bold">
-          Contact
-        </h2>
+        <h2 className="text-4xl font-bold">Contact</h2>
 
         <p className="pt-6 pb-6 text-base max-w-2xl text-center m-auto dark:text-neutral-400">
-          Want to contact us? Choose an option below and well be happy to show you how Halley can transform her companys web experience.</p>
+          Want to contact us? Choose an option below and well be happy to show
+          you how Halley can transform her companys web experience.
+        </p>
       </div>
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-8 pb-16 grid md:grid-cols-2 lg:grid-cols-2 gap-y-8 md:gap-x-8 md:gap-y-8 lg:gap-x-8 lg:gap-y-16">
-
         <div>
           <h2 className="text-lg font-bold">Contact Halley</h2>
-          <p className="max-w-sm mt-4 mb-4 dark:text-neutral-400">Have something to say? We are here to help. Fill up the form or send email or call phone.</p>
+          <p className="max-w-sm mt-4 mb-4 dark:text-neutral-400">
+            Have something to say? We are here to help. Fill up the form or send
+            email or call phone.
+          </p>
 
           <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-neutral-400">
             <HiOutlineLocationMarker className="w-4 h-4" />
@@ -79,17 +81,12 @@ export default function Contact() {
 
           <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-neutral-400">
             <MdOutlineMail className="w-4 h-4" />
-            <a href="mailto:hello@halley.vercel.app">
-              hello@halley.vercel.app
-            </a>
+            <a href="mailto:hello@halley.vercel.app">hello@halley.vercel.app</a>
           </div>
-
 
           <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-neutral-400">
             <HiOutlinePhone className="w-4 h-4" />
-            <a href="tel:51931999999">
-              +51 931999999
-            </a>
+            <a href="tel:51931999999">+51 931999999</a>
           </div>
         </div>
 
@@ -101,10 +98,7 @@ export default function Contact() {
                 value="dbf01db4-76b8-4845-98d6-b120f7e13245"
                 {...register("access_key")}
               />
-              <input
-                type="hidden"
-                {...register("subject")}
-              />
+              <input type="hidden" {...register("subject")} />
               <input
                 type="hidden"
                 value="Mission Control"
@@ -115,17 +109,19 @@ export default function Contact() {
                 id=""
                 className="hidden"
                 style={{ display: "none" }}
-                {...register("botcheck")}></input>
+                {...register("botcheck")}
+              ></input>
 
               <div className="mb-5">
                 <input
                   type="text"
                   placeholder="Full Name"
                   autoComplete="false"
-                  className={`w-full px-4 py-3 border-2 placeholder:text-neutral-800 dark:text-white rounded-md outline-none dark:placeholder:text-neutral-200 dark:bg-neutral-900 focus:ring-4 ${errors.name
-                    ? "border-rose-500 focus:border-rose-500 ring-rose-100 dark:ring-0"
-                    : "border-neutral-300 focus:border-neutral-600 ring-neutral-100 dark:border-neutral-600 dark:focus:border-white dark:ring-0"
-                    }`}
+                  className={`w-full px-4 py-3 border-2 placeholder:text-neutral-800 dark:text-white rounded-md outline-none dark:placeholder:text-neutral-200 dark:bg-neutral-900 focus:ring-4 ${
+                    errors.name
+                      ? "border-rose-500 focus:border-rose-500 ring-rose-100 dark:ring-0"
+                      : "border-neutral-300 focus:border-neutral-600 ring-neutral-100 dark:border-neutral-600 dark:focus:border-white dark:ring-0"
+                  }`}
                   {...register("name", {
                     required: "Full name is required",
                     maxLength: 80,
@@ -148,10 +144,11 @@ export default function Contact() {
                   placeholder="Email Address"
                   name="email"
                   autoComplete="false"
-                  className={`w-full px-4 py-3 border-2 placeholder:text-neutral-800 dark:text-white rounded-md outline-none dark:placeholder:text-neutral-200 dark:bg-neutral-900   focus:ring-4  ${errors.email
-                    ? "border-rose-500 focus:border-rose-500 ring-rose-100 dark:ring-0"
-                    : "border-neutral-300 focus:border-neutral-600 ring-neutral-100 dark:border-neutral-600 dark:focus:border-white dark:ring-0"
-                    }`}
+                  className={`w-full px-4 py-3 border-2 placeholder:text-neutral-800 dark:text-white rounded-md outline-none dark:placeholder:text-neutral-200 dark:bg-neutral-900   focus:ring-4  ${
+                    errors.email
+                      ? "border-rose-500 focus:border-rose-500 ring-rose-100 dark:ring-0"
+                      : "border-neutral-300 focus:border-neutral-600 ring-neutral-100 dark:border-neutral-600 dark:focus:border-white dark:ring-0"
+                  }`}
                   {...register("email", {
                     required: "Enter your email",
                     pattern: {
@@ -171,10 +168,11 @@ export default function Contact() {
                 <textarea
                   name="message"
                   placeholder="Your Message"
-                  className={`w-full px-4 py-3 border-2 placeholder:text-neutral-800 dark:text-white dark:placeholder:text-neutral-200 dark:bg-neutral-900   rounded-md outline-none  h-36 focus:ring-4  ${errors.message
-                    ? "border-rose-500 focus:border-rose-500 ring-rose-100 dark:ring-0"
-                    : "border-neutral-300 focus:border-neutral-600 ring-neutral-100 dark:border-neutral-600 dark:focus:border-white dark:ring-0"
-                    }`}
+                  className={`w-full px-4 py-3 border-2 placeholder:text-neutral-800 dark:text-white dark:placeholder:text-neutral-200 dark:bg-neutral-900   rounded-md outline-none  h-36 focus:ring-4  ${
+                    errors.message
+                      ? "border-rose-500 focus:border-rose-500 ring-rose-100 dark:ring-0"
+                      : "border-neutral-300 focus:border-neutral-600 ring-neutral-100 dark:border-neutral-600 dark:focus:border-white dark:ring-0"
+                  }`}
                   {...register("message", { required: "Enter your Message" })}
                 />
                 {errors.message && (
@@ -187,24 +185,28 @@ export default function Contact() {
 
               <button
                 type="submit"
-                className="w-full py-4 font-semibold text-white transition-colors bg-neutral-900 rounded-md hover:bg-neutral-800 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-neutral-200 px-7 dark:bg-white dark:text-black ">
+                className="w-full py-4 font-semibold text-white transition-colors bg-neutral-900 rounded-md hover:bg-neutral-800 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-neutral-200 px-7 dark:bg-white dark:text-black "
+              >
                 {isSubmitting ? (
                   <svg
                     className="w-5 h-5 mx-auto text-white dark:text-neutral-900 animate-spin"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
-                    viewBox="0 0 24 24">
+                    viewBox="0 0 24 24"
+                  >
                     <circle
                       className="opacity-25"
                       cx="12"
                       cy="12"
                       r="10"
                       stroke="currentColor"
-                      strokeWidth="4"></circle>
+                      strokeWidth="4"
+                    ></circle>
                     <path
                       className="opacity-75"
                       fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
                   </svg>
                 ) : (
                   "Send Message"
@@ -222,18 +224,24 @@ export default function Contact() {
                   className="text-green-500"
                   viewBox="0 0 100 100"
                   fill="none"
-                  xmlns="http://www.w3.org/2000/svg">
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path
                     d="M26.6666 50L46.6666 66.6667L73.3333 33.3333M50 96.6667C43.8716 96.6667 37.8033 95.4596 32.1414 93.1144C26.4796 90.7692 21.3351 87.3317 17.0017 82.9983C12.6683 78.6649 9.23082 73.5204 6.8856 67.8586C4.54038 62.1967 3.33331 56.1283 3.33331 50C3.33331 43.8716 4.54038 37.8033 6.8856 32.1414C9.23082 26.4796 12.6683 21.3351 17.0017 17.0017C21.3351 12.6683 26.4796 9.23084 32.1414 6.88562C37.8033 4.5404 43.8716 3.33333 50 3.33333C62.3767 3.33333 74.2466 8.24998 82.9983 17.0017C91.75 25.7534 96.6666 37.6232 96.6666 50C96.6666 62.3768 91.75 74.2466 82.9983 82.9983C74.2466 91.75 62.3767 96.6667 50 96.6667Z"
                     stroke="currentColor"
                     strokeWidth="3"
                   />
                 </svg>
-                <h3 className="py-5 text-2xl font-medium text-green-500">Success</h3>
-                <p className="text-neutral-900 dark:text-neutral-300 md:px-4">{Message}</p>
+                <h3 className="py-5 text-2xl font-medium text-green-500">
+                  Success
+                </h3>
+                <p className="text-neutral-900 dark:text-neutral-300 md:px-4">
+                  {Message}
+                </p>
                 <button
                   className="mt-6 py-2 px-4 bg-rose-500 rounded-full focus:outline-none text-neutral-100"
-                  onClick={() => reset()}>
+                  onClick={() => reset()}
+                >
                   Go back
                 </button>
               </div>
@@ -248,7 +256,8 @@ export default function Contact() {
                 viewBox="0 0 97 97"
                 className="text-rose-500"
                 fill="none"
-                xmlns="http://www.w3.org/2000/svg">
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path
                   d="M27.9995 69C43.6205 53.379 52.3786 44.621 67.9995 29M26.8077 29L67.9995 69M48.2189 95C42.0906 95 36.0222 93.7929 30.3604 91.4477C24.6985 89.1025 19.554 85.6651 15.2206 81.3316C10.8872 76.9982 7.44975 71.8538 5.10454 66.1919C2.75932 60.53 1.55225 54.4617 1.55225 48.3333C1.55225 42.205 2.75932 36.1366 5.10454 30.4748C7.44975 24.8129 10.8872 19.6684 15.2206 15.335C19.554 11.0016 24.6985 7.56418 30.3604 5.21896C36.0222 2.87374 42.0906 1.66667 48.2189 1.66667C60.5957 1.66667 72.4655 6.58333 81.2172 15.335C89.9689 24.0867 94.8856 35.9566 94.8856 48.3333C94.8856 60.7101 89.9689 72.58 81.2172 81.3316C72.4655 90.0833 60.5957 95 48.2189 95Z"
                   stroke="CurrentColor"
@@ -259,16 +268,19 @@ export default function Contact() {
               <h3 className=" py-5 text-2xl font-medium text-rose-500">
                 Oops, Something went wrong!
               </h3>
-              <p className="text-neutral-900 dark:text-neutral-300 md:px-4">{Message}</p>
-              <button className="mt-6 py-2 px-4 bg-rose-500 rounded-full focus:outline-none text-neutral-100" onClick={() => reset()}>
+              <p className="text-neutral-900 dark:text-neutral-300 md:px-4">
+                {Message}
+              </p>
+              <button
+                className="mt-6 py-2 px-4 bg-rose-500 rounded-full focus:outline-none text-neutral-100"
+                onClick={() => reset()}
+              >
                 Try Again
               </button>
             </div>
           )}
-
         </div>
       </div>
-    </div >
+    </div>
   );
 }
-
