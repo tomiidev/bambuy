f3134f2bb4dfbf1e404aac19fd10cb5395f513a9
Humber Sanchez Nieto <husani925@gmail.com>
2022-11-12 22:00:50 -0500

    correction dark mode

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -67,7 +67,7 @@ export default function Contact() {
       });
   };
   return (
-    <div className="bg-gray-50 dark:bg-neutral-800" id="contact">
+    <div className="bg-gray-50 dark:bg-neutral-900" id="contact">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 text-center">
         <h2 className="text-4xl font-bold">Contact</h2>
 
