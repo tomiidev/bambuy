68878a4ae1eb768bc0c7c83fcd838a1c67f38d06
tomiidev <tomasruglio21@gmail.com>
2024-05-29 18:45:56 -0300

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7c8ceab99ca4aea0"; }
+/******/ 		__webpack_require__.h = function() { return "4737cd656e49d068"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
